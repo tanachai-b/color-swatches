120a613c548a2a98bab79dfe93449ee2c33724a6
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-22 04:08:44 +0700

    developers: add Noto Sans fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans", "sans-serif"],
+        mono: ["Noto Sans Mono", "monospace"],
+      },
+    },
+  },
   plugins: [],
 };
