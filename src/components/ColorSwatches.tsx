ab466c64296cdcc1a395d189d6cb9f8c1df06fff
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-29 16:00:01 +0700

    developers: move getTextColor to separate folder

--- src/components/ColorSwatches.tsx
+++ src/components/ColorSwatches.tsx
@@ -1,4 +1,5 @@
 import cx from "classnames";
+import { getTextColor } from "src/functions";
 
 export function ColorSwatches({ colorRows }: { colorRows: string[][] }) {
   return (
@@ -36,14 +37,4 @@ function ColorSwatch({ color }: { color: string }) {
       {color}
     </div>
   );
-}
-
-function getTextColor(color: string) {
-  const red = parseInt(color.substring(1, 3), 16) / 255;
-  const green = parseInt(color.substring(3, 5), 16) / 255;
-  const blue = parseInt(color.substring(5, 7), 16) / 255;
-
-  const brightness = (blue + red * 2 + green * 4) / 7;
-
-  return brightness > 0.5 ? "#000000" : "#ffffff";
 }
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,2 @@
 export * from "./getColorRows";
+export * from "./getTextColor";
