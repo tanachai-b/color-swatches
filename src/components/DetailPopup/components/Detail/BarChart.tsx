f465a408a25d694bda76455ea9702a6ee2a14580
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-30 02:36:31 +0700

    developers: re-organize DetailPopup Detail files and folders

--- src/components/DetailPopup/components/Detail/BarChart.tsx
+++ src/components/DetailPopup/components/Detail/BarChart.tsx
@@ -1,60 +1,19 @@
 import cx from "classnames";
 import { ReactNode } from "react";
 
-export function Detail({ color }: { color?: string }) {
-  const { r, g, b } = rgb(color ?? "#000000");
-  const { h, s, v } = hsv(color ?? "#000000");
-
+export function BarChart({ children }: { children: ReactNode }) {
   return (
-    <div
-      className={cx(
-        "p-[20px]",
-        "py-[30px]",
-        "gap-[30px]",
-
-        "flex",
-        "flex-col",
-
-        "text-[#ffffff80]",
-        "text-[13px]",
-      )}
-    >
-      <div className={cx("grid", "grid-flow-row", "grid-cols-[auto,1fr,auto]", "gap-[10px]")}>
-        <Label>Red</Label>
-        <Bar value={r / 255} />
-        <Value value={r} isInteger={true} />
-
-        <Label>Green</Label>
-        <Bar value={g / 255} />
-        <Value value={g} isInteger={true} />
-
-        <Label>Blue</Label>
-        <Bar value={b / 255} />
-        <Value value={b} isInteger={true} />
-      </div>
-
-      <div className={cx("grid", "grid-flow-row", "grid-cols-[auto,1fr,auto]", "gap-[10px]")}>
-        <Label>Hue</Label>
-        <Bar value={h / 360} />
-        <Value value={h} unit={"°"} />
-
-        <Label>Saturation</Label>
-        <Bar value={s / 100} />
-        <Value value={s} unit={"%"} />
-
-        <Label>Brightness</Label>
-        <Bar value={v / 100} />
-        <Value value={v} unit={"%"} />
-      </div>
+    <div className={cx("grid", "grid-flow-row", "grid-cols-[auto,1fr,auto]", "gap-[10px]")}>
+      {children}
     </div>
   );
 }
 
-function Label({ children }: { children: ReactNode }) {
+export function Label({ children }: { children: ReactNode }) {
   return <div className={cx("w-[9ch]", "text-right")}>{children}</div>;
 }
 
-function Bar({ value }: { value: number }) {
+export function Bar({ value }: { value: number }) {
   return (
     <div className={cx("grid", "items-center", "relative")}>
       <div className={cx("w-full", "h-[5px]", "bg-[#ffffff10]", "rounded-full")} />
@@ -73,7 +32,15 @@ function Bar({ value }: { value: number }) {
   );
 }
 
-function Value({ value, isInteger, unit }: { value: number; isInteger?: boolean; unit?: string }) {
+export function Value({
+  value,
+  isInteger,
+  unit,
+}: {
+  value: number;
+  isInteger?: boolean;
+  unit?: string;
+}) {
   const fix = value.toFixed(1).split(".");
   const num = fix[0];
   const dec = fix[1];
@@ -89,7 +56,7 @@ function Value({ value, isInteger, unit }: { value: number; isInteger?: boolean;
   );
 }
 
-function rgb(hex: string) {
+export function rgb(hex: string) {
   const r = parseInt(hex.substring(1, 3), 16);
   const g = parseInt(hex.substring(3, 5), 16);
   const b = parseInt(hex.substring(5, 7), 16);
@@ -97,7 +64,7 @@ function rgb(hex: string) {
   return { r, g, b };
 }
 
-function hsv(hex: string) {
+export function hsv(hex: string) {
   const { r, g, b } = rgb(hex);
 
   const min = Math.min(r, g, b);
---
+++ src/components/DetailPopup/components/Detail/Detail.tsx
@@ -0,0 +1,51 @@
+import cx from "classnames";
+import { Bar, BarChart, Label, Value, hsv, rgb } from "./BarChart";
+
+export function Detail({ color }: { color?: string }) {
+  const { r, g, b } = rgb(color ?? "#000000");
+  const { h, s, v } = hsv(color ?? "#000000");
+
+  return (
+    <div
+      className={cx(
+        "p-[20px]",
+        "py-[30px]",
+        "gap-[30px]",
+
+        "flex",
+        "flex-col",
+
+        "text-[#ffffff80]",
+        "text-[13px]",
+      )}
+    >
+      <BarChart>
+        <Label>Red</Label>
+        <Bar value={r / 255} />
+        <Value value={r} isInteger={true} />
+
+        <Label>Green</Label>
+        <Bar value={g / 255} />
+        <Value value={g} isInteger={true} />
+
+        <Label>Blue</Label>
+        <Bar value={b / 255} />
+        <Value value={b} isInteger={true} />
+      </BarChart>
+
+      <BarChart>
+        <Label>Hue</Label>
+        <Bar value={h / 360} />
+        <Value value={h} unit={"°"} />
+
+        <Label>Saturation</Label>
+        <Bar value={s / 100} />
+        <Value value={s} unit={"%"} />
+
+        <Label>Brightness</Label>
+        <Bar value={v / 100} />
+        <Value value={v} unit={"%"} />
+      </BarChart>
+    </div>
+  );
+}
