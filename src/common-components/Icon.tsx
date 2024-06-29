9f0efe792223e10a9a307db7618c06fae164722b
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-29 16:00:17 +0700

    developers: add fontVariationSettings to Icon component

--- src/common-components/Icon.tsx
+++ src/common-components/Icon.tsx
@@ -8,7 +8,13 @@ export function Icon({ icon }: { icon: string }) {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
 
-      <span className={cx("material-symbols-rounded")} style={{ fontSize: "inherit" }}>
+      <span
+        className={cx("material-symbols-rounded")}
+        style={{
+          fontVariationSettings: "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24",
+          fontSize: "inherit",
+        }}
+      >
         {icon}
       </span>
     </>
