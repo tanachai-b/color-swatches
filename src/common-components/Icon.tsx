72e947794916d6fa595649c7958c04b3a63fda7f
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-06-30 21:49:51 +0700

    bug fix: material icon show text while loading

--- src/common-components/Icon.tsx
+++ src/common-components/Icon.tsx
@@ -2,21 +2,15 @@ import cx from "classnames";
 
 export function Icon({ icon }: { icon: string }) {
   return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-      />
-
-      <span
-        className={cx("material-symbols-rounded")}
-        style={{
-          fontVariationSettings: "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24",
-          fontSize: "inherit",
-        }}
-      >
-        {icon}
-      </span>
-    </>
+    <span
+      className={cx("material-symbols-rounded")}
+      style={{
+        fontVariationSettings: "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24",
+        fontSize: "inherit",
+        width: "1ch",
+      }}
+    >
+      {icon}
+    </span>
   );
 }
